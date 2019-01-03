d6056d4ff3a66c282a834d34fb10eb1738aaaf96
Harman Bains <bains.hmn@gmail.com>
2019-01-02 21:52:47 -0800

    initial mongoDB connection in playground folder

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,38 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if (err) {
+    console.log('Unable to connect to MongoDB server');
+  } else {
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').insertOne({
+    //   text: 'Something to do',
+    //   completed: false
+    // }, (err, result) => {
+    //   if (err) {
+    //     console.log('Unable to insert todo', err);
+    //   } else {
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //   }
+    //
+    // });
+
+    db.collection('Users').insertOne({
+      name: 'Harman Bains',
+      age: 29,
+      location: 'Earth'
+    }, (err, result) => {
+      if (err) {
+        console.log('Unable to insert user', err);
+      } else {
+        console.log(JSON.stringify(result.ops, undefined, 2));
+      }
+
+    });
+
+    client.close();
+  }
+
+});
