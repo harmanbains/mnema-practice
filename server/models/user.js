852eef865be21de7fca8cdb274531a14afbdd140
Harman Bains <bains.hmn@gmail.com>
2019-01-14 20:09:42 -0800

    improved user model and added POST /users

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,42 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+// {
+//     email: 'bains.hmn@gmail.com',
+//     password: 'myPass123',
+//     tokens: [{
+//       access: 'auth'
+//       token: ''
+//     }]
+// }
 
 var User = mongoose.model('User', {
   email: {
     type: String,
     required: true,
     minlength: 1,
-    trim: true
-  }
+    trim: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      required: true
+    },
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 });
 
 module.exports = {User};
